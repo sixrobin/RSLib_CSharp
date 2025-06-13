a558cdf3f9db45215b2568b6162b0a921865d415
Robin Six <55784799+sixrobin@users.noreply.github.com>
2025-06-13 23:31:40 +0200

    String.ToSnakeCase extension

--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -29,5 +29,31 @@ namespace RSLib.CSharp.Extensions
         
             return newText.ToString().ToUpper();
         }
+        
+        /// <summary>
+        /// Converts a string to its snake case version.
+        /// For instance, AttackSpeedMultiplier would become attack_speed_multiplier.
+        /// Does not take acronyms into account.
+        /// </summary>
+        /// <param name="s">String to convert.</param>
+        /// <returns>Converted string.</returns>
+        public static string ToSnakeCase(this string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+                return "";
+        
+            StringBuilder newText = new(s.Length * 2);
+            newText.Append(s[0]);
+        
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (char.IsUpper(s[i]))
+                    newText.Append('_');
+            
+                newText.Append(s[i]);
+            }
+        
+            return newText.ToString().ToLower();
+        }
     }
 }
