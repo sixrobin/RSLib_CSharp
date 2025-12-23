6d6774e42003785c3bf76bb00dba80373737e5a9
Robin Six <55784799+sixrobin@users.noreply.github.com>
2025-12-23 14:58:05 +0100

    Added dictionary GetRandomKey extensions

--- Extensions/DictionaryExtensions.cs
+++ Extensions/DictionaryExtensions.cs
@@ -1,6 +1,7 @@
 ﻿namespace RSLib.CSharp.Extensions
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     public static class DictionaryExtensions
     {
@@ -20,7 +21,7 @@
         /// <summary>
         /// Checks the value for key TKey and returns it, or returns the default value for TValue if key was not found.
         /// </summary>
-        /// <param name="dict">Dictionary get the value in, if contained.</param>
+        /// <param name="dict">Dictionary to get the value in, if contained.</param>
         /// <param name="key">Key to look for.</param>
         /// <param name="customDefault">Custom default value to use in case key is not found.</param>
         /// <returns>Found existing value, or default TValue value.</returns>
@@ -32,7 +33,7 @@
         /// <summary>
         /// Checks the value for key TKey and returns it, or returns the default value for TValue and triggers a callback if key was not found.
         /// </summary>
-        /// <param name="dict">Dictionary get the value in, if contained.</param>
+        /// <param name="dict">Dictionary to get the value in, if contained.</param>
         /// <param name="key">Key to look for.</param>
         /// <param name="defaultUsedCallback">Callback triggered if key is not found.</param>
         /// <param name="customDefault">Custom default value to use in case key is not found.</param>
@@ -65,7 +66,7 @@
         /// <summary>
         /// Checks the value for key TKey and returns it, or creates a new pair and triggers a callback if key was not found.
         /// </summary>
-        /// <param name="dict">Dictionary get the value in if contained or insert new instance.</param>
+        /// <param name="dict">Dictionary to get the value in if contained or insert new instance.</param>
         /// <param name="key">Key to look for.</param>
         /// <param name="insertNewCallback">Callback triggered if key is not found.</param>
         /// <returns>Found existing value, or created one.</returns>
@@ -83,5 +84,72 @@
         }
 
         #endregion // GENERAL
+        
+        #region RANDOM
+        
+        /// <summary>
+        /// Gets a random key using values as weights.
+        /// </summary>
+        /// <param name="dict">Dictionary to get a random key from.</param>
+        /// <returns>Randomly selected key.</returns>
+        public static TKey GetRandomKey<TKey>(this Dictionary<TKey, int> dict)
+        {
+            int totalWeight = dict.Sum(kvp => kvp.Value);
+            double randomWeight = new System.Random().NextDouble() * totalWeight;
+            double currentWeight = 0;
+
+            foreach (KeyValuePair<TKey, int> kvp in dict)
+            {
+                currentWeight += kvp.Value;
+                if (currentWeight >= randomWeight)
+                    return kvp.Key;
+            }
+
+            return dict.Keys.Last();
+        }
+        
+        /// <summary>
+        /// Gets a random key using values as weights.
+        /// </summary>
+        /// <param name="dict">Dictionary to get a random key from.</param>
+        /// <returns>Randomly selected key.</returns>
+        public static TKey GetRandomKey<TKey>(this Dictionary<TKey, float> dict)
+        {
+            float totalWeight = dict.Sum(kvp => kvp.Value);
+            double randomWeight = new System.Random().NextDouble() * totalWeight;
+            double currentWeight = 0;
+
+            foreach (KeyValuePair<TKey, float> kvp in dict)
+            {
+                currentWeight += kvp.Value;
+                if (currentWeight >= randomWeight)
+                    return kvp.Key;
+            }
+
+            return dict.Keys.Last();
+        }
+        
+        /// <summary>
+        /// Gets a random key using values as weights.
+        /// </summary>
+        /// <param name="dict">Dictionary to get a random key from.</param>
+        /// <returns>Randomly selected key.</returns>
+        public static TKey GetRandomKey<TKey>(this Dictionary<TKey, double> dict)
+        {
+            double totalWeight = dict.Sum(kvp => kvp.Value);
+            double randomWeight = new System.Random().NextDouble() * totalWeight;
+            double currentWeight = 0;
+
+            foreach (KeyValuePair<TKey, double> kvp in dict)
+            {
+                currentWeight += kvp.Value;
+                if (currentWeight >= randomWeight)
+                    return kvp.Key;
+            }
+
+            return dict.Keys.Last();
+        }
+        
+        #endregion // RANDOM
     }
 }
